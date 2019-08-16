0b2fa9df3f60da0ad0906cfa136d4f0208c92156
Oleksandr Khomenko <homes89@ukr.net>
2019-08-16 22:09:15 +0300

    Fix prompts

--- src/scripts/generator/config.js
+++ src/scripts/generator/config.js
@@ -1,105 +1,106 @@
 const listComponents = require("./listComponents");
+const componentsPath = "../../components/";
 
 /**
  * Generate React component for an app
  */
 
 module.exports = {
-    description: "Generate a new React component",
-    prompts: [
-      {
-            type: "list",
-            name: "action",
-            message: "Select action",
-            choices: () => [
-                {
-                    name:
-                        "Create (Create component folder with Component.js and index.js files)",
-                    value: "create"
-                },
-                {
-                    name:
-                        "Add (Add new Component.js file inside of existing component folder)",
-                    value: "add"
-                }
-            ]
+  description: "Generate a new React component",
+  prompts: [
+    {
+      type: "list",
+      name: "action",
+      message: "Select action",
+      choices: () => [
+        {
+          name:
+            "Create (Create component folder with Component.js and index.js files)",
+          value: "create"
         },
         {
-            type: "list",
-            name: "component",
-            message: "Select component",
-            when: answer => answer.action === "add",
-            choices: listComponents,
-            filter: val => val.toLowerCase()
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "Component name:",
-            validate: value => {
-                if (!value) {
-                    return "Component name is required";
-                }
-                return true;
-            }
-        },
-        {
-            type: "list",
-            name: "type",
-            message: "Select component type",
-            default: "Stateless Function",
-            choices: () => ["Stateless Function", "Component", "PureComponent"]
+          name:
+            "Add (Add new Component.js file inside of existing component folder)",
+          value: "add"
         }
-    ],
-    actions: data => {
-        let template = "./templates/class.js.hbs";
-        let actions = [];
-        let path =
-            "../../{{app}}/components/{{properCase name}}/{{properCase name}}.js";
-
-        if (data.type === "Stateless Function") {
-            template = "./templates/functional.js.hbs";
+      ]
+    },
+    {
+      type: "list",
+      name: "component",
+      message: "Select component",
+      when: answer => answer.action === "add",
+      choices: listComponents,
+      filter: value => value.toLowerCase()
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Component name:",
+      validate: value => {
+        if (!value) {
+          return "Component name is required";
         }
-
-        if (data.action === "add") {
-            path = `../../{{app}}/components/{{properCase component}}/{{properCase name}}.js`;
-            actions = [
-                {
-                    type: "append",
-                    path:
-                        "../../{{app}}/components/{{properCase component}}/index.js",
-                    templateFile: "./templates/index.js.hbs"
-                }
-            ];
-        }
-
-        actions = [
-            ...actions,
-            {
-                type: "add",
-                path: path,
-                templateFile: template
-            }
-        ];
-
-        if (data.action === "create") {
-            actions = [
-                ...actions,
-                {
-                    type: "add",
-                    path:
-                        "../../{{app}}/components/{{properCase name}}/index.js",
-                    templateFile: "./templates/index.js.hbs"
-                },
-                {
-                    type: "add",
-                    path:
-                        "../../{{app}}/components/{{properCase name}}/messages.js",
-                    templateFile: "./templates/messages.js.hbs"
-                }
-            ];
-        }
-
-        return actions;
+        return true;
+      }
+    },
+    {
+      type: "list",
+      name: "type",
+      message: "Select component type",
+      default: "Functional component",
+      choices: () => [
+        "Functional component",
+        "Class Based Component",
+        "Pure Component"
+      ]
     }
+  ],
+  actions: data => {
+    let template = "./templates/class.js.hbs";
+    let actions = [];
+    let path = "../../components/{{properCase name}}/{{properCase name}}.js";
+
+    if (data.type === "Stateless Function") {
+      template = "./templates/functional.js.hbs";
+    }
+
+    if (data.action === "add") {
+      path = `../../components/{{properCase component}}/{{properCase name}}.js`;
+      actions = [
+        {
+          type: "append",
+          path: "../../{{app}}/components/{{properCase component}}/index.js",
+          templateFile: "./templates/index.js.hbs"
+        }
+      ];
+    }
+
+    actions = [
+      ...actions,
+      {
+        type: "add",
+        path: path,
+        templateFile: template
+      }
+    ];
+
+    if (data.action === "create") {
+      actions = [
+        ...actions,
+        {
+          type: "add",
+          path: "../../{{app}}/components/{{properCase name}}/index.js",
+          templateFile: "./templates/index.js.hbs"
+        },
+        {
+          type: "add",
+          path: "../../{{app}}/components/{{properCase name}}/messages.js",
+          templateFile: "./templates/messages.js.hbs"
+        }
+      ];
+    }
+
+    return actions;
+  }
 };
--- src/scripts/generator/listComponents.js
+++ src/scripts/generator/listComponents.js
@@ -2,8 +2,5 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = () => {
-    const names = fs.readdirSync(
-        path.join(__dirname, `../../components`)
-    );
-    return names//.filter(name => name !== ".DS_Store");
+  return fs.readdirSync(path.join(__dirname, `../../components`));
 };
