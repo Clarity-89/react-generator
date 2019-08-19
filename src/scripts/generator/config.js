2e458df2c9903372dccd892e6f600c38237e19d6
Oleksandr Khomenko <homes89@ukr.net>
2019-08-19 20:48:51 +0300

    Add redux prompts

--- src/scripts/generator/config.js
+++ src/scripts/generator/config.js
@@ -10,6 +10,15 @@ module.exports = {
   prompts: [
     {
       type: "list",
+      name: "select",
+      choices: () => [
+        { name: "React Component", value: "react_component" },
+        { name: "Redux Action", value: "redux_action" }
+      ]
+    },
+
+    {
+      type: "list",
       name: "action",
       message: "Select action",
       choices: () => [
@@ -50,6 +59,76 @@ module.exports = {
         { name: "Functional component", value: "functional" },
         { name: "Class Based Component", value: "class" }
       ]
+    },
+    {
+      type: "list",
+      name: "create_or_modify",
+      message: "Do you want to create a new action or modify an existing one?",
+      when: answer => answer.select === "redux_action",
+      choices: () => [
+        {
+          name: "Create (will create new actions file)",
+          value: "create"
+        },
+        {
+          name: "Modify (will add the action to an existing one) ",
+          value: "modify"
+        }
+      ]
+    },
+    {
+      type: "list",
+      name: "action",
+      message: "Select action folder",
+      when: ({ select, create_or_modify }) => {
+        return select === "redux_action" && create_or_modify === "modify";
+      },
+      choices: listComponents("actions"),
+      filter: value => value.toLowerCase()
+    },
+    {
+      type: "input",
+      name: "action_prefix",
+      message: "Action prefix (e.g. 'post', 'user'):",
+      when: ({ select, create_or_modify }) =>
+        select === "redux_action" && create_or_modify === "create",
+      validate: value => {
+        if (!value) {
+          return "A name is required";
+        }
+        return true;
+      }
+    },
+    {
+      type: "input",
+      name: "action_name",
+      message: "Action name:",
+      when: answer => answer.select === "redux_action",
+      validate: value => {
+        if (!value) {
+          return "A name is required";
+        }
+        return true;
+      }
+    },
+    {
+      type: "confirm",
+      name: "reducer_confirm",
+      message: "Do you want to import actions into reducer?",
+      when: ({ select }) => select === "redux_action"
+    },
+    {
+      type: "list",
+      name: "reducer_name",
+      choices: listComponents("reducers"),
+      when: ({ select, create_or_modify, reducer_confirm }) => {
+        return (
+          select === "redux_action" &&
+          create_or_modify === "modify" &&
+          reducer_confirm
+        );
+      },
+      message: "Select reducer"
     }
   ],
   actions: data => {
